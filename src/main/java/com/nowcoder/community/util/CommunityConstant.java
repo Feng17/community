840ba82aab9a1e3a0437e46c8f52d92d49e6fd0b
feng17 <shippuden7@163.com>
2020-02-10 00:53:21 +0800

    帖子及评论回复详情页

---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,16 @@
+package com.nowcoder.community.util;
+
+public interface CommunityConstant {
+
+
+    /**
+     * 实体类型: 帖子
+     */
+    int ENTITY_TYPE_POST = 1;
+
+    /**
+     * 实体类型: 评论
+     */
+    int ENTITY_TYPE_COMMENT = 2;
+
+}
