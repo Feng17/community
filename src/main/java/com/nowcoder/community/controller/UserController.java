ec4a013f0ae31e753abbaac89c84b93e575ccb50
feng17 <shippuden7@163.com>
2020-02-16 01:46:35 +0800

    收到的赞

---
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.annotation.LoginRequired;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.LikeService;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.HostHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class UserController implements CommunityConstant {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private LikeService likeService;
+
+
+
+    @LoginRequired
+    @RequestMapping(path = "/setting", method = RequestMethod.GET)
+    public String getSettingPage() {
+        return "/site/setting";
+    }
+
+
+    // 个人主页
+    @RequestMapping(path = "/profile/{userId}", method = RequestMethod.GET)
+    public String getProfilePage(@PathVariable("userId") int userId, Model model) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("该用户不存在!");
+        }
+
+        // 用户
+        model.addAttribute("user", user);
+        // 点赞数量
+        int likeCount = likeService.findUserLikeCount(userId);
+        model.addAttribute("likeCount", likeCount);
+
+        return "/site/profile";
+    }
+
+}
--- src/main/java/com/nowcoder/community/service/LikeService.java
+++ src/main/java/com/nowcoder/community/service/LikeService.java
@@ -2,7 +2,10 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.util.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,17 +16,39 @@ public class LikeService {
 
     // 点赞
     public void like(int userId, int entityType, int entityId, int entityUserId) {
+//
+//        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+//        String userLikeKey = RedisKeyUtil.getUserLikeKey(entityUserId);
+//
+//        boolean isMember = redisTemplate.opsForSet().isMember(entityLikeKey, userId);
+//
+//        if (isMember) {
+//            redisTemplate.opsForSet().remove(entityLikeKey, userId);
+//        } else {
+//            redisTemplate.opsForSet().add(entityLikeKey, userId);
+//        }
 
-        String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
-        String userLikeKey = RedisKeyUtil.getUserLikeKey(entityUserId);
+        redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String entityLikeKey = RedisKeyUtil.getEntityLikeKey(entityType, entityId);
+                String userLikeKey = RedisKeyUtil.getUserLikeKey(entityUserId);
 
-        boolean isMember = redisTemplate.opsForSet().isMember(entityLikeKey, userId);
+                boolean isMember = operations.opsForSet().isMember(entityLikeKey, userId);
 
-        if (isMember) {
-            redisTemplate.opsForSet().remove(entityLikeKey, userId);
-        } else {
-            redisTemplate.opsForSet().add(entityLikeKey, userId);
-        }
+                operations.multi();
+
+                if (isMember) {
+                    operations.opsForSet().remove(entityLikeKey, userId);
+                    operations.opsForValue().decrement(userLikeKey);
+                } else {
+                    operations.opsForSet().add(entityLikeKey, userId);
+                    operations.opsForValue().increment(userLikeKey);
+                }
+
+                return operations.exec();
+            }
+        });
 
     }
 
@@ -39,11 +64,15 @@ public class LikeService {
         return redisTemplate.opsForSet().isMember(entityLikeKey, userId) ? 1 : 0;
     }
 
+
+
     // 查询某个用户获得的赞
     public int findUserLikeCount(int userId) {
         String userLikeKey = RedisKeyUtil.getUserLikeKey(userId);
         Integer count = (Integer) redisTemplate.opsForValue().get(userLikeKey);
         return count == null ? 0 : count.intValue();
     }
+
+
 
 }
