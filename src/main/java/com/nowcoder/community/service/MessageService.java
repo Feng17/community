d54481ac9dfa01da346335028039bb474cb7f931
feng17 <shippuden7@163.com>
2020-02-21 12:35:00 +0800

    发送系统通知

---
+++ src/main/java/com/nowcoder/community/service/MessageService.java
@@ -0,0 +1,44 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.MessageMapper;
+import com.nowcoder.community.entity.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
+
+import java.util.List;
+
+@Service
+public class MessageService {
+
+    @Autowired
+    private MessageMapper messageMapper;
+
+
+
+    public int addMessage(Message message) {
+        message.setContent(HtmlUtils.htmlEscape(message.getContent()));
+        return messageMapper.insertMessage(message);
+    }
+
+    public int readMessage(List<Integer> ids) {
+        return messageMapper.updateStatus(ids, 1);
+    }
+
+    public Message findLatestNotice(int userId, String topic) {
+        return messageMapper.selectLatestNotice(userId, topic);
+    }
+
+    public int findNoticeCount(int userId, String topic) {
+        return messageMapper.selectNoticeCount(userId, topic);
+    }
+
+    public int findNoticeUnreadCount(int userId, String topic) {
+        return messageMapper.selectNoticeUnreadCount(userId, topic);
+    }
+
+    public List<Message> findNotices(int userId, String topic, int offset, int limit) {
+        return messageMapper.selectNotices(userId, topic, offset, limit);
+    }
+
+}
